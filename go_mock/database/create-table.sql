ba9a595632b1faa03177bbf53501bdfce63ee727
hvn2706 <62832264+hvn2706@users.noreply.github.com>
2023-02-10 11:13:33 +0700

    Extend field type limit in database

--- go_mock/database/create-table.sql
+++ go_mock/database/create-table.sql
@@ -3,7 +3,7 @@ CREATE TABLE album (
   id         INT AUTO_INCREMENT NOT NULL,
   title      VARCHAR(128) NOT NULL,
   artist     VARCHAR(255) NOT NULL,
-  price      DECIMAL(5,2) NOT NULL,
+  price      DECIMAL(10,2) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
